b8a34af95b9d2abe8a5114f66d0c12905e0717c9
grzesiek <javapopolsku@gmail.com>
2012-05-23 19:22:30 +0200

    App

---
+++ test/test-simple/src/main/java/jpp/test/simple/App.java
@@ -0,0 +1,13 @@
+package jpp.test.simple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
